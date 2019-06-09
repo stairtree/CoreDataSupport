0e63e86ddb53707133e6edf69b59cac8ef5651d8
Thomas Krajacic <thomas@krajacic.at>
2019-06-09 15:25:19 +0200

    Clean up

--- Package.swift
+++ Package.swift
@@ -1,23 +1,18 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CoreDataSupport",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CoreDataSupport",
             targets: ["CoreDataSupport"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
          .package(url: "git@gitlab.com:stairtree/frameworks/Utilities.git", from: "3.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CoreDataSupport",
             dependencies: ["Utilities"]),
